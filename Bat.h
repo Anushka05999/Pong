f57f2564de97005df971695b6ae433590f0f3207
Anushka <anushkasinha0210@gmail.com>
2025-09-14 19:00:32 +0530

    Update Bat.h

--- Bat.h
+++ Bat.h
@@ -1,23 +1,23 @@
-#include<SFML/Graphics.hpp>
-using namespace sf;
+#include<SFML/Graphics.hpp>// include SFML graphics module
+using namespace sf;//allows using SFML classes directly (e.g., Vector2f, RectangleShape)
  class Bat{
   
 private:
-        Vector2f m_Position;
-          RectangleShape m_Shape;
-          float m_Speed=600.0f;
-          bool m_movingRight=false;
-          bool m_movingLeft=false;
+          Vector2f m_Position; // stores x, y position of the bat
+          RectangleShape m_Shape;// rectangle representing the bat
+          float m_Speed=600.0f; // bat movement speed in pixels per second
+          bool m_movingRight=false; // flag to check if bat is moving right
+          bool m_movingLeft=false;// flag to check if bat is moving left
           
 public:
-        Bat(float startX,float startY); 
+        Bat(float startX,float startY);  // constructor (initializes bat position and shape)
         
-         FloatRect getPosition();
-         RectangleShape getShape();        
+         FloatRect getPosition();// returns bat boundary box for collision detection
+         RectangleShape getShape();// returns bat shape for rendering
          
-         void moveLeft();
-         void moveRight();
-         void stopLeft();
-         void stopRight();
-         void update(Time dt);
+         void moveLeft(); // sets left movement flag
+         void moveRight(); // sets right movement flag
+         void stopLeft();// clears left movement flag
+         void stopRight();// clears right movement flag
+         void update(Time dt);// updates bat’s position using delta time
          };
